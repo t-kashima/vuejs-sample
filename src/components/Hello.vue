20a85641523ec0d9ff1fb8dc7c61d9377c376d2d
Takumi KASHIMA <kodokurider@gmail.com>
2017-12-23 15:23:12 +0900

    init

---
+++ src/components/Hello.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data () {
+    return {
+      msg: 'Hello, world'
+    }
+  }
+}
+</script>
